20e41aab6d833ffb890529b960c8293028b805a9
Cindy Lin <cindy0976@gmail.com>
2020-03-15 01:41:13 +0800

    Some changes

--- script.js
+++ script.js
@@ -2,26 +2,33 @@ $(document).ready(function () {
     let addButton = $(".add-button");
 	let field = $(".add-field");
     let tasks = $(".tasks");
+    let input = $("input");
+   
+    function inputLength(){
+        return input.val().length;
+    }
 
     //Create callback function 
     function addElement(){
-        let elem = `<li class="task">
-                <div class="task-checked col-1">
-                    <input type="checkbox">
-                </div>
-                <div class="task-text col-2">${field.val()}</div>
-                <div class="task-remove col-3"></div>
-            </li>`;
-            tasks.append(elem);
-            field.val(null);
+        if(inputLength()>0){
+            let elem = `<li class="task">
+            <div class="task-checked col-1">
+                <input type="checkbox">
+            </div>
+            <div class="task-text col-2">${field.val()}</div>
+            <div class="task-remove col-3"></div>
+        </li>`;
+        tasks.append(elem);
+        field.val(null);
+        }
     }
 
     // Add tasks by click      
     addButton.on("click", addElement);
 
     //Add tasks by keypress
-    $("input").on("keypress", function(e){
-            if(e.which===13 ){
+    input.on("keypress", function(e){
+            if(inputLength()>0 && e.which===13){
                 addElement();
             }
     });
